3ee4e10136b4af39e43370961755fddc1b02052a
Niharika Menon <080300nic@gmail.com>
2021-09-01 16:01:04 +0530

    changes

--- zoom-login-scrpit/login.py
+++ zoom-login-scrpit/login.py
@@ -2,13 +2,12 @@ import pyautogui as gui
 import time
 import json
 
-#Hack to quickly press a keyboard key
 def keypress(gui,code):
     gui.keyDown(code)
     gui.keyUp(code)
 
 print("Opening Zoom")
-#Open Zoom:
+#To open ZOOM
 gui.keyDown("winleft")
 gui.keyUp("winleft")
 time.sleep(5)
@@ -19,8 +18,8 @@ time.sleep(10)
 print("Done")
 
 
-#Resize zoom window to make it top left corner only.
-print("Resizing The window to fit base conditions")
+#Resize zoom
+print("Resizing The window ")
 gui.keyDown("winleft")
 cheat = ["up","up","down","left","up"]
 for code in cheat:
@@ -29,21 +28,17 @@ gui.keyUp("winleft")
 time.sleep(2)
 print("Done")
 
-
-#For 1920x1080, click on this co-ords to get the join button
 print("Clicking Join Meeting")
 gui.click(331,254)
 time.sleep(3)
 print("Done")
 
 
-#Load meeting details
+# meeting details
 print("Loading Data from File")
 data = json.load(open('cmeeting.json'))
 print("Done")
 
-
-#Writing details to screen
 print("Writing Meeting ID and Name")
 gui.write(str(data["M_Id"]))
 keypress(gui,"tab")
@@ -74,7 +69,3 @@ keypress(gui,"tab")
 keypress(gui,"space")
 print("Done")
 print("Entered Zoom Meeting")
-
-#Look at pip schedule!
-#work on a method to make take a new meeting input.
-#work on a method to schedule a meeting.
